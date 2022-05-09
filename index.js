ac8e310bce154764823eccfb5ff8cdd4abd4a79e
Rocco Parrella <businessroccoparrella@gmail.com>
2022-05-09 12:38:46 -0300

    Prueba

--- index.js
+++ index.js
@@ -19,38 +19,41 @@ module.exports = (async () => {
         const chat = require('./socket/index');
 
 
-        mongoose.connect("mongodb+srv://paella:M1o2n3g4o5@ecommerce.gazzm.mongodb.net/ecommerce?retryWrites=true&w=majority").then(() => {
-            initializePassport(passport)
+        app.get('/', (req, res) => {
+            res.sendStatus(200);
+        })
+        // mongoose.connect("mongodb+srv://paella:M1o2n3g4o5@ecommerce.gazzm.mongodb.net/ecommerce?retryWrites=true&w=majority").then(() => {
+        //     initializePassport(passport)
             
-            engine(app);
-            app.use(express.json());
-            app.use(express.urlencoded({ extended: true }));
-            app.use(flash());
-            app.use(session({
-                secret: 'secret',
-                resave: true,
-                saveUninitialized: true,
-                store: new mongoStore({
-                    mongoUrl: process.env.MONGOURL,
-                    ttl: 10 * 60,
-                    expires: 1000 * 60 * 10,
-                    autoRemove: "native"
-                })
-            }))
-            app.use(passport.initialize());
-            app.use(passport.session());
-            app.use("/static", express.static(path.join(__dirname, 'public')))
+        //     engine(app);
+        //     app.use(express.json());
+        //     app.use(express.urlencoded({ extended: true }));
+        //     app.use(flash());
+        //     app.use(session({
+        //         secret: 'secret',
+        //         resave: true,
+        //         saveUninitialized: true,
+        //         store: new mongoStore({
+        //             mongoUrl: process.env.MONGOURL,
+        //             ttl: 10 * 60,
+        //             expires: 1000 * 60 * 10,
+        //             autoRemove: "native"
+        //         })
+        //     }))
+        //     app.use(passport.initialize());
+        //     app.use(passport.session());
+        //     app.use("/static", express.static(path.join(__dirname, 'public')))
 
-            io.on('connection', chat);
-            app.use('/', router);
+        //     io.on('connection', chat);
+        //     app.use('/', router);
 
-            server.on('error', (err) => {
-                console.log(err);
-            });
+        //     server.on('error', (err) => {
+        //         console.log(err);
+        //     });
 
-        }).catch(err => {
-            console.log(err);
-        });
+        // }).catch(err => {
+        //     console.log(err);
+        // });
 
         return server;
 })()
